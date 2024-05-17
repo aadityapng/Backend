0816058977a29baf21b3ed3cdba494e8f2b4e08a
aadityapng <aditya.pangestu46@gmail.com>
2024-05-17 22:31:04 +0700

    perbarui seeder

--- seeders/20240406120850-menu-categories.js
+++ seeders/20240406120850-menu-categories.js
@@ -3,17 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const now = new Date();
-
     return queryInterface.bulkInsert("MasterMenuCategories", [
       {
-        name: "Category 1",
-        created_at: now,
+        name: "Food",
+        created_at: new Date(),
         created_by: 1,
       },
       {
-        name: "Category 2",
-        created_at: now,
+        name: "Drink",
+        created_at: new Date(),
         created_by: 1,
       },
     ]);
