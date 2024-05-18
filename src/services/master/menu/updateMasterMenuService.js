daabec4e774228bcae52e3b87ab32b8b59932c99
Arinal Dzikrul <arinal.dzikrul@sinarmasmining.com>
2024-05-18 12:54:54 +0700

    update script menu edit

--- src/services/master/menu/updateMasterMenuService.js
+++ src/services/master/menu/updateMasterMenuService.js
@@ -2,33 +2,35 @@ const MasterMenu = require("../../../models/menu");
 const { uploadCloudinary } = require("../../../helper/index");
 
 module.exports = async (req, res) => {
+  const { id } = req.params;
   req.body.updateAt = new Date().toDateString();
   req.body.updateBy = req.userId;
 
-  const url = await uploadCloudinary(req.file.path);
-  if (url) {
-    req.body.menu_photo = url.toString();
-    const { category_id, name, price, updateAt, updateBy, menu_photo } =
-      req.body;
-
-    try {
-      return await MasterMenu.update(
-        {
-          category_id,
-          name,
-          price,
-          menu_photo,
-          updateAt,
-          updateBy,
-        },
-        { where: { id } }
-      );
-    } catch (error) {
-      return res.status(500).send({ message: error });
+  if (req.file) {
+    const url = await uploadCloudinary(req.file.path);
+    if (url) {
+      req.body.menu_photo = url.toString();
     }
   } else {
-    return res.json({
-      message: "error when upload to cloudinary",
-    });
+    const menu = await MasterMenu.findOne({ where: { id } });
+    req.body.menu_photo = menu.menu_photo;
+  }
+
+  const { category_id, name, price, updateAt, updateBy, menu_photo } = req.body;
+
+  try {
+    return await MasterMenu.update(
+      {
+        category_id,
+        name,
+        price,
+        menu_photo,
+        updateAt,
+        updateBy,
+      },
+      { where: { id } }
+    );
+  } catch (error) {
+    return res.status(500).send({ message: error });
   }
 };
