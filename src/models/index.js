921ce42aeec872f51242b57a8edab766d363f0f1
Oka Krisna <7776987-Okakrisna@users.noreply.gitlab.com>
2024-05-19 15:15:04 +0800

    be

--- src/models/index.js
+++ src/models/index.js
@@ -1,26 +1,28 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: "postgres",
-    });
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
 
-    const db = {};
+const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.menu = require('./menu')
-db.menu_category = require('./masterMenuCategories')
-db.orderDetail = require('./DetailOrderTransaction')
+db.menu = require("./menu");
+db.menu_category = require("./masterMenuCategories");
+db.orderDetail = require("./DetailOrderTransaction");
 
-db.menu.hasMany(db.menu_category, { foreignKey: 'id' })
-db.menu_category.belongsTo(db.menu, { foreignKey: 'category_id' })
-db.menu.hasMany(db.orderDetail, { foreignKey: 'menu_id' })
+db.menu.hasOne(db.menu_category, { foreignKey: "id" });
+db.menu_category.belongsTo(db.menu, { foreignKey: "category_id" });
+db.menu.hasMany(db.orderDetail, { foreignKey: "menu_id" });
 db.orderDetail.belongsTo(db.menu, {
-    foreignKey: 'menu_id'
-})
+  foreignKey: "menu_id",
+});
 
-module.exports = db
+module.exports = db;
